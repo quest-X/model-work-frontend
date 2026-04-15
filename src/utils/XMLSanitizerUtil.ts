d7fa6741ae80e9854df0c4f0ecd77444e3dceaeb
Law <law@777.local>
2026-04-15 11:27:25 +0800

    fix: XMLSanitizerUtil escape ampersand first, use global replace

--- src/utils/XMLSanitizerUtil.ts
+++ src/utils/XMLSanitizerUtil.ts
@@ -1,10 +1,10 @@
 export class XMLSanitizerUtil {
     public static sanitize(input: string): string {
         return input
-            .replace('<', '&lt;')
-            .replace('>', '&gt;')
-            .replace('&', '&amp;')
-            .replace("'", '&#39;')
-            .replace("/", '&#x2F;')
+            .replace(/&/g, '&amp;')
+            .replace(/</g, '&lt;')
+            .replace(/>/g, '&gt;')
+            .replace(/'/g, '&#39;')
+            .replace(/\//g, '&#x2F;');
     }
 }
